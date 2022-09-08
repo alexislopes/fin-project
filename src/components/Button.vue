ab9d2d4717c7e8cf5fb2dcd207da28417f9ad9e7
Alexis Lopes <alexisdrops@gmail.com>
2022-09-07 22:40:57 -0300

    adiciona suporte a criacao/atualizacao de objetivos

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,19 +1,21 @@
 <template>
-<button class=" p-2 rounded-lg h-fit font-poppins bg-slate-200 shadow-md font-medium">
-{{label}}
-</button>
+  <button class="p-2 rounded-lg h-fit font-poppins bg-slate-200 shadow-md font-medium">
+    {{label}}
+  </button>
 </template>
 
 <script setup lang="ts">
- 
- defineProps({
-  label: {type: String}
- });
+
+defineProps({
+  label: { type: String }
+});
 
 
 
 </script>
 
 <style>
-
+button:disabled {
+  @apply opacity-50 cursor-not-allowed
+}
 </style>
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,21 +1,23 @@
 <template>
-<div class="rounded-md" style="background-color: rgb(209 213 219)">
-      <label class="font-poppins flex flex-col p-2 text-sm font-medium gap-2" for="nome">
-        {{label}}
-        <input :value="modelValue" @input="updateValue" class="outline-none text-base bg-transparent" type="text" id="nome">
-      </label>
-    </div>
+  <div class="rounded-md" style="background-color: rgb(209 213 219)">
+    <label class="font-poppins flex flex-col p-2 text-sm font-medium gap-2" for="nome">
+      {{label}}
+      <input :autocomplete="autocomplete ? 'on' : 'off'" :value="modelValue" @input="updateValue"
+        class="outline-none text-base bg-transparent" :sstype="type" id="nome">
+    </label>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 defineProps({
-  label: {type: String, required: false},
-  modelValue: {type: String, required: false}
+  label: { type: String, required: false },
+  modelValue: { type: [String, Number], required: false },
+  type: { type: String, default: 'text' },
+  autocomplete: { type: Boolean, default: false }
 })
 
 const emit = defineEmits(['update:modelValue', 'input'])
 const updateValue = (event) => {
-console.log("🚀 ~ file: Input.vue ~ line 18 ~ updateValue ~ event", event)
   emit('update:modelValue', event.target.value)
   emit('input', event.target.value)
 }
